526049a9b71464ad3682d84a310b0b3711b70551
Tim Kozemzak <timkozemzak@gmail.com>
2021-02-11 17:01:29 -0700

    work on fixing card, minor styling updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,40 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-import "../App.css"
+import "../App.css";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 const Header = ({ currentUser }) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-
-        <div className={classes.root}>
+  return (
+    <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-        {<Avatar alt="User" src={currentUser.avatar_url} />}
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          {<Avatar alt="User" src={currentUser.avatar_url} />}
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
@@ -40,8 +44,7 @@ const Header = ({ currentUser }) => {
         </Toolbar>
       </AppBar>
     </div>
-        
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
