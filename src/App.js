a1ec7fff83082caa0298fce1eb94a3439eb4b1c9
Tim Kozemzak <timkozemzak@gmail.com>
2021-02-23 19:33:05 -0700

    update endpoints for keystore-proxy-server

--- src/App.js
+++ src/App.js
@@ -1,85 +1,79 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import token from './token';
-import Repositories from './components/Repositories.js'
-import Header from './components/Header.js';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Repositories from "./components/Repositories.js";
+import Header from "./components/Header.js";
 
 function App() {
-  
   const [currentUser, setCurrentUser] = useState([]);
   const [currentUserExists, setCurrentUserExists] = useState(false);
   const [repos, setRepos] = useState([]);
 
-  
-
-  var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer " + token);
-    console.log("headers:", myHeaders);
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-  
-    
   //   useEffect(() => {
   //     const getRepos = async () => {
   //       const reposFromApi = await fetchRepos()
   //       setRepos(reposFromApi)
-        
+
   //     }
   //   getRepos()
   // }, [])
 
-  //fetch user from github api
+  //fetch user from github api (through keystore-proxy-server)
   const fetchUser = async () => {
-    const res = await fetch(`https://api.github.com/users/${currentUser}`, requestOptions)
-    const data = await res.json()
-    .then((data) => {
-      setCurrentUser(data)
-      setCurrentUserExists(true)
-    }).then(() => {
-      fetchRepos()
-      }) 
-  }
+    const res = await fetch(
+      `http://localhost:3000/api/v1/project-wishlist/${currentUser}`
+    );
+    const data = await res
+      .json()
+      .then((data) => {
+        setCurrentUser(data);
+        setCurrentUserExists(true);
+      })
+      .then(() => {
+        fetchRepos();
+      });
+  };
 
-  //fetch repos from github api
+  //fetch repos from github api (through keystore-proxy-server)
   const fetchRepos = async () => {
-    const res = await fetch(`https://api.github.com/users/${currentUser}/repos`, requestOptions)
-    const data = await res.json()
-    setRepos(data)
-    return data
-
-  }
-
+    const res = await fetch(
+      `http://localhost:3000/api/v1/project-wishlist/${currentUser}/repos`
+    );
+    const data = await res.json();
+    setRepos(data);
+    return data;
+  };
 
   const handleUserEntry = (e) => {
-    setCurrentUser(e.target.value)
-  }
+    setCurrentUser(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchUser()
-  }
+    fetchUser();
+  };
 
-  const enteredValue = '';
-  
+  const enteredValue = "";
 
   return (
     <div className="App">
-      <Header currentUser={currentUser}/>
-      {
-      currentUserExists ?
-      <Repositories repos={repos}/> :
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>
-          <input type="text" name="user" placeholder="Enter Github Username" value={currentUser} onChange={handleUserEntry}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      }
+      <Header currentUser={currentUser} />
+      {currentUserExists ? (
+        <Repositories repos={repos} />
+      ) : (
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label>
+            <input
+              type="text"
+              name="user"
+              placeholder="Enter Github Username"
+              value={currentUser}
+              onChange={handleUserEntry}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      )}
     </div>
-
   );
 }
 
